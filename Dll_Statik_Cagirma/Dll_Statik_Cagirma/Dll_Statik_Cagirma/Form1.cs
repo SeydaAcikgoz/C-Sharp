b8607f9bd9f05d957d89ab165688a83346cb4d97
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-04-19 20:55:30 +0300

    Add files via upload

---
+++ Dll_Statik_Cagirma/Dll_Statik_Cagirma/Dll_Statik_Cagirma/Form1.cs
@@ -0,0 +1,27 @@
+﻿using Dll_ornek_projesi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dll_Statik_Cagirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hesaplama hesaplama = new Hesaplama();
+            SonucTxt.Text= hesaplama.islem_hesapla(Convert.ToInt32(Sayi1Txt.Text), Convert.ToInt32(Sayi2Txt.Text)).ToString();
+        }
+    }
+}
